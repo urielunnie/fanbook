496f1bf07aa9574799f7d11614707e608e511a53
unknown <khusnulmafiroh241@gmail.com>
2023-05-25 11:15:59 +0700

    Second Commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from os.path import join, dirname
+import os
+from audioop import add
+
+MONGODB_URI = os.environ.get(
+    "mongodb+srv://test:sparta@cluster0.sfozobg.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("dbsparta")
+
+client = MongoClient(
+    'mongodb+srv://test:sparta@cluster0.sfozobg.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+app = Flask(__name__)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route("/homework", methods=["POST"])
+def homework_post():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+    doc = {
+        'name': name_receive,
+        'comment': comment_receive,
+    }
+    db.fanmessages.insert_one(doc)
+    return jsonify({'msg':'POST request!'})
+
+@app.route("/homework", methods=["GET"])
+def homework_get():
+    message_list = list(db.fanmessages.find({}, {'_id': False}))
+    return jsonify({'messages': message_list})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
---
+++ start.sh
@@ -0,0 +1,18 @@
+set -eu
+
+export PYTHONUNBUFFERED=true
+
+VIRTUALENV=.data/venv
+
+if [ ! -d SVIRTUALENV ]; then
+    python3 -m venv $VIRTUALENV
+fi
+
+if [ ! -f SVIRTUALENV/bin/pip ]; then
+    curl -silent-show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | SVIRTUALE
+fi
+
+SVIRTUALENV/bin/pip install -r requirements.txt
+
+SVIRTUALENV/bin/python3 app.py
+Footer
